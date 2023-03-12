556d7e52ee605c37b4cffdf5d7cc6bca908ee9ed
Walter213 <46505629+Walter213@users.noreply.github.com>
2023-03-11 19:05:23 -0700

    Update CreateSchedule.sql

--- CreateSchedule.sql
+++ CreateSchedule.sql
@@ -1,36 +1,44 @@
-USE [MOYO_DB]
-GO
-
-/****** Object:  StoredProcedure [dbo].[CreateSchedule]    Script Date: 2023-02-22 1:47:00 PM ******/
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
-GO
-
--- =============================================
--- Author:		<Author,,Name>
--- Create date: <Create Date,,>
--- Description:	<Description,,>
--- =============================================
-CREATE PROCEDURE [dbo].[CreateSchedule] 
-	-- Add the parameters for the stored procedure here
-	--@CalenderID [int] IDENTITY(1,1) NOT NULL,
-	@AppointmentDate date,
-	@AppointmentStartDate time(7), 
-	@AppointmentEndDate time(7),
-	@StaffID int,
-	@TotalNumberOfParticipants int,
-	@Status bit
-AS
-BEGIN
-	SET NOCOUNT ON;
-    -- Insert statements for procedure here
-	INSERT INTO Schedule
-              (AppointmentDate, AppointmentStartDate, AppointmentEndDate, StaffID, TotalNumberOfParticipants, [Status])
-       VALUES
-              (@AppointmentDate, @AppointmentStartDate, @AppointmentEndDate, @StaffID, @TotalNumberOfParticipants, @Status)
-END
-GO
-
-
+create or alter procedure CreateSchedule (@AppointmentDate date, @AppointmentStartTime time, @AppointmentEndTime time, @StaffID int, @MaxParticipants int)
+as
+	Declare @ReturnCode int
+	set @ReturnCode = 1
+if @AppointmentDate is NULL
+	Begin
+		RAISERROR('CreateSchedule - @AppointmentDate is Required', 16, 1)
+	End
+Else
+	 if @AppointmentStartTime is NULL
+		Begin
+			RAISERROR('CreateSchedule - @AppointmentStartDate is Required', 16, 1)
+		End
+	 Else
+		if @AppointmentEndTime is NULL
+			Begin
+				RAISERROR('CreateSchedule - @AppointmentEndDate is Required', 16, 1)
+			End
+		Else
+			if @StaffID is NULL
+				Begin
+					RAISERROR('CreateSchedule - @StaffID is Required', 16, 1)
+				End
+				if not exists (select StaffID from Staff where StaffID = @StaffID)
+					Begin
+						RAISERROR('CreateSchedule - Staff Member does not exist', 16, 1)
+					End
+				Else
+					if @MaxParticipants is NULL
+						Begin
+							RAISERROR('CreateSchedule - @MaxParticipants is Required', 16, 1)
+						End
+							Else
+								Begin
+									insert into Schedule(StaffID, AppointmentDate, AppointmentStartDate, AppointmentEndDate, TotalNumberOfParticipants, MaxParticipants, Status)
+												values (@StaffID, @AppointmentDate, @AppointmentStartTime, @AppointmentEndTime, 0, @MaxParticipants, 1)
+													if @@ERROR = 0
+														set @ReturnCode = 0
+													Else
+														RAISERROR('CreateSchedule - CreateSchedule Error from Schedule Table', 16, 1)
+												End
+														
+												
+Return @ReturnCode
